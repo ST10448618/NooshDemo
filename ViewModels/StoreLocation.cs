071403e34b45886434a4142b1dd6698c3702141a
Perezr12 <ST10448618@vcconnect.edu.za>
2026-07-26 12:16:40 +0200

    Update Store Info

--- ViewModels/StoreLocation.cs
+++ ViewModels/StoreLocation.cs
@@ -12,6 +12,7 @@ namespace NooshApp.ViewModels
         public string PhoneNumber { get; set; } = string.Empty;
         public string WhatsAppNumber { get; set; } = string.Empty;
         public string OpeningHours { get; set; } = string.Empty;
+        public List<string> HoursSchedule { get; set; } = new();
         public string GoogleMapsUrl { get; set; } = string.Empty;
         public string MapEmbedUrl { get; set; } = string.Empty;
         public string DirectionsUrl =>
